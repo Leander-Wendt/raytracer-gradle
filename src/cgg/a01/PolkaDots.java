5140baf2dab6dd7a04f6e74bfc7b90270d50268e
Leander Wendt <s52888@beuth-hochschule.de>
2021-10-12 11:33:55 +0200

    build constructor

--- src/cgg/a01/PolkaDots.java
+++ src/cgg/a01/PolkaDots.java
@@ -1,18 +1,23 @@
 package cgg.a01;
 
 import cgtools.*;
+import java.util.ArrayList;
 
 // Represents the contents of an image. Provides the same color for all pixels.
 public class PolkaDots implements Sampler {
   Color bgColor, cColor;
   double cx, cy, r;
+  int row, column;
 
-  public PolkaDots(Color bgColor, Color cColor, double width, double height, double r) {
+  public PolkaDots(Color bgColor, Color cColor, double width, double height, double r, int amount) {
     this.bgColor = bgColor;
     this.cColor = cColor;
     cx = width / 2;
     cy = height / 2;
     this.r = r;
+    row = getDivider(amount);
+    column = amount / row;
+    if (row > column) swap(row, column);
   }
 
   // Returns the color for the given position.
@@ -22,5 +27,20 @@ public class PolkaDots implements Sampler {
     double d = Math.pow( dx + dy, (double) 1/2);
     if (d <= r) return cColor;
     return bgColor;
+  }
+
+  private int getDivider (int num) {
+    ArrayList<Integer> temp = new ArrayList<>();
+    for (int i = 1; i <= num; i++){
+        if (num % i == 0) 
+            temp.add(i);
+    }
+    return temp.get(temp.size());
+  }
+
+  private void swap (int a, int b){
+    int temp = a;
+    a = b;
+    b = temp;
   }
 }
